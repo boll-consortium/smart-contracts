a5e46a1daf659100378e1e697fd2b8e203d11a32
patrick <ocheja.patrick@gmail.com>
2019-01-26 18:20:40 +0900

    code cleanup

--- Registrar.sol
+++ Registrar.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.17;
 contract Registrar {
     event RegistrarContractEvents(address indexed sender, address indexed participantAddress,
         address affectedContractAddress, string indexed actionType);
+
     struct Participant {
         address ethAddress;
         bool isLearningProvider;
@@ -15,19 +16,19 @@ contract Registrar {
         bytes accessToken;
         mapping(bytes => address) schoolId2BOLLAddress;
     }
-    
+
     address owner;
     mapping(address => Participant) private registeredParticipants;
     mapping(address => SchoolsUsersRegistry) private schoolsUsersRegistry;
-    
+
     constructor() public {
         owner = msg.sender;
-    emit RegistrarContractEvents(msg.sender, msg.sender, address(this), "RC");
+        emit RegistrarContractEvents(msg.sender, msg.sender, address(this), "RC");
     }
 
-    function findUserAddress(bytes userId, address schoolBOLLAddress, bytes accessToken) public view returns(address) {
-        if(isApprovedInstitute(accessToken)){
-            if(schoolsUsersRegistry[schoolBOLLAddress].isApproved) {
+    function findUserAddress(bytes userId, address schoolBOLLAddress, bytes accessToken) public view returns (address) {
+        if (isApprovedInstitute(accessToken)) {
+            if (schoolsUsersRegistry[schoolBOLLAddress].isApproved) {
                 //emit RegistrarContractEvents(msg.sender, schoolBOLLAddress, address(this), "findUserAddress");
                 return schoolsUsersRegistry[schoolBOLLAddress].schoolId2BOLLAddress[userId];
             }
@@ -35,20 +36,20 @@ contract Registrar {
         }
     }
 
-    function findUserAddress(bytes userId, bytes accessToken) public view returns(address) {
+    function findUserAddress(bytes userId, bytes accessToken) public view returns (address) {
         return findUserAddress(userId, msg.sender, accessToken);
     }
 
-function getIndexContract(address participantAddress) public constant returns (address) {
-    //emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "getIndexContract");
+    function getIndexContract(address participantAddress) public constant returns (address) {
+        //emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "getIndexContract");
         return registeredParticipants[participantAddress].indexContract;
     }
-    
+
     function getStatus(address participantAddress) public constant returns (uint8) {
         //emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "getStatus");
         return registeredParticipants[participantAddress].status;
     }
-    
+
     function checkIsLearningProvider(address participantAddress) public constant returns (bool) {
         //emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "checkIsLearningProvider");
         return registeredParticipants[participantAddress].isLearningProvider;
@@ -56,49 +57,47 @@ function getIndexContract(address participantAddress) public constant returns (a
 
     // ToDo who can deregister a user?
     function unregistered(address participantAddress, bytes accessToken) public payable returns (bool) {
-        if (isApprovedInstitute(accessToken) && registeredParticipants[participantAddress].status ==1) {
+        if (isApprovedInstitute(accessToken) && registeredParticipants[participantAddress].status == 1) {
             registeredParticipants[participantAddress].status = 3;
             emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "unregistered");
             return true;
-        }else {
+        } else {
             return false;
         }
     }
     // must be sent by the school ToDo change to allow schools to register other schools via consensus
     function approveInstitute(address schoolAddress, bool approved, bytes accessToken) public payable {
-        if(msg.sender == owner) {
-        schoolsUsersRegistry[schoolAddress] = SchoolsUsersRegistry(approved, accessToken);
+        if (msg.sender == owner) {
+            schoolsUsersRegistry[schoolAddress] = SchoolsUsersRegistry(approved, accessToken);
             emit RegistrarContractEvents(msg.sender, schoolAddress, address(this), "approveInstitute");
         }
     }
     //
     function register(address participantAddress, bool isLearningProvider, address indexContract, bytes accessToken) public payable returns (bool) {
-        if(isApprovedInstitute(accessToken)){
+        if (isApprovedInstitute(accessToken)) {
             Participant memory participant = registeredParticipants[participantAddress];
             // Needs rewrite because only the creator can register learners
-           if ((msg.sender == participantAddress && isLearningProvider) ||
-            (msg.sender != participantAddress && !isLearningProvider) ) {
-               if (participant.ethAddress == 0) {
+            if ((msg.sender == participantAddress && isLearningProvider) ||
+                (msg.sender != participantAddress && !isLearningProvider)) {
+                if (participant.ethAddress == 0) {
                     participant.ethAddress = participantAddress;
-                    participant.isLearningProvider = isLearningProvider;
-                    participant.indexContract = indexContract;
-                    participant.status = 1;
-                } else {
-                    //already registered;
-                    participant.isLearningProvider = isLearningProvider;
-                    participant.status = 1;
-                    participant.indexContract = indexContract;
                 }
+
+                participant.isLearningProvider = isLearningProvider;
+                participant.status = 1;
+                participant.indexContract = indexContract;
+
                 registeredParticipants[participantAddress] = participant;
-               emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "register");
+                emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "register");
+
                 return true;
-           }
+            }
         }
-       return false;
+        return false;
     }
     // must be sent/signed by school
     function pairUserIdWithBOLLAddress(bytes userId, address bollAddress, bytes accessToken) public payable {
-        if(isApprovedInstitute(accessToken)) {
+        if (isApprovedInstitute(accessToken)) {
             schoolsUsersRegistry[msg.sender].schoolId2BOLLAddress[userId] = bollAddress;
             emit RegistrarContractEvents(msg.sender, bollAddress, address(this), "pairUserIdWithBOLLAddress");
         }
@@ -110,18 +109,18 @@ function getIndexContract(address participantAddress) public constant returns (a
             registeredParticipants[participantAddress].indexContract = indexContract;
             emit RegistrarContractEvents(msg.sender, participantAddress, address(this), "assignIndexContract");
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
-    function isApprovedInstitute(bytes accessToken) public view returns(bool) {
+    function isApprovedInstitute(bytes accessToken) public view returns (bool) {
         //emit RegistrarContractEvents(msg.sender, msg.sender, address(this), "isApprovedInstitute");
         return schoolsUsersRegistry[msg.sender].isApproved &&
-          keccak256(schoolsUsersRegistry[msg.sender].accessToken) == keccak256(accessToken);
+        keccak256(schoolsUsersRegistry[msg.sender].accessToken) == keccak256(accessToken);
     }
 
-    function isApprovedInstitute(bytes accessToken, address providerAddress) public view returns(bool) {
+    function isApprovedInstitute(bytes accessToken, address providerAddress) public view returns (bool) {
         //emit RegistrarContractEvents(msg.sender, providerAddress, address(this), "isApprovedInstitute");
         return schoolsUsersRegistry[providerAddress].isApproved &&
         keccak256(schoolsUsersRegistry[providerAddress].accessToken) == keccak256(accessToken);
